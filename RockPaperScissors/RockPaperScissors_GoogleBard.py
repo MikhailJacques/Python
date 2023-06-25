0e6cd78d5bb6cae0e9a9f80b652fecaabdf662be
Mikhail Jacques <mikhailjacques@gmail.com>
2023-06-25 19:20:46 +0300

    Update

---
+++ RockPaperScissors/RockPaperScissors_GoogleBard.py
@@ -0,0 +1,56 @@
+# # Game of rock, paper, scissors (Google Bard generated)
+# import random
+#
+#
+# def get_user_choice():
+#     while True:
+#         user_choice = input("Choose rock, paper, or scissors: ").lower()
+#         if user_choice in ["rock", "paper", "scissors"]:
+#             return user_choice
+#         else:
+#             print("Invalid choice. Please try again.")
+#
+#
+# def get_computer_choice():
+#     return random.choice(["rock", "paper", "scissors"])
+#
+#
+# def determine_winner(user_choice, computer_choice):
+#     if user_choice == computer_choice:
+#         return "Tie"
+#     elif user_choice == "rock" and computer_choice == "scissors":
+#         return "User"
+#     elif user_choice == "paper" and computer_choice == "rock":
+#         return "User"
+#     elif user_choice == "scissors" and computer_choice == "paper":
+#         return "User"
+#     else:
+#         return "Computer"
+#
+#
+# def main():
+#     while True:
+#         user_choice = get_user_choice()
+#         computer_choice = get_computer_choice()
+#         winner = determine_winner(user_choice, computer_choice)
+#
+#         print(f"Your choice: {user_choice}, Computer's choice: {computer_choice}")
+#         if winner == "User":
+#             print("You win!")
+#         elif winner == "Computer":
+#             print("Computer wins!")
+#         else:
+#             print("Tie!")
+#
+#         play_again = input("Do you want to play again? (y/n): ").lower()
+#         if play_again in ["y", "n"]:
+#             if play_again == "y":
+#                 continue
+#             else:
+#                 break
+#         else:
+#             print("Invalid input. Please enter 'y' or 'n': ")
+#
+#
+# if __name__ == "__main__":
+#     main()
---
+++ TicTacToe/TicTacToe.py
@@ -0,0 +1,77 @@
+# # This is a sample Python script.
+#
+# # Press Shift+F10 to execute it or replace it with your code.
+# # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+def print_board(board):
+    for row in board:
+        print("|".join(row))
+        print("-" * 5)
+
+
+def is_winner(board, player):
+    # Check rows, columns and diagonals for a win
+    return (any(all(cell == player for cell in row) for row in board) or
+            any(all(row[i] == player for row in board) for i in range(3)) or
+            all(board[i][i] == player for i in range(3)) or
+            all(board[i][2 - i] == player for i in range(3)))
+
+
+def is_full(board):
+    return all(cell != ' ' for row in board for cell in row)
+
+
+def tic_tac_toe():
+    # Initialize the board
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+
+    while True:
+        print_board(board)
+
+        # Get the player's move
+        try:
+            row, col = map(int,
+                           input(f"Player {current_player}, enter row and column (1-3) separated by space: ").split())
+        except ValueError:
+            print("Please enter valid numbers.")
+            continue
+
+        # Check if the move is valid
+        if row < 1 or row > 3 or col < 1 or col > 3 or board[row - 1][col - 1] != ' ':
+            print("Invalid move. Try again.")
+            continue
+
+        # Make the move
+        board[row - 1][col - 1] = current_player
+
+        # Check for a win or tie
+        if is_winner(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        elif is_full(board):
+            print_board(board)
+            print("It's a tie!")
+            break
+
+        # Switch the current player
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+if __name__ == "__main__":
+    tic_tac_toe()
+
